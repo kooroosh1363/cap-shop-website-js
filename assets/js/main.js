47747078831ea17d97ad5bfa6caaba76a03c2cae
kooroosh1363 <peymanraad1363@gmail.com>
2023-08-05 23:19:04 +0400

    add var colors in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
 var img = document.querySelector("#main-cap");
+var black = document.querySelector(".black");
+var red = document.querySelector(".red");
+var green = document.querySelector(".green");
+var sand = document.querySelector(".sand");
+var grayWhite = document.querySelector(".gray-white");
+var white = document.querySelector(".white");
+
+const color = document.querySelectorAll(".color");
+
+
+// shop product info
+var info = [
+    {
+        src:"../img/36W-black.png"
+    },
+    {
+        src: "../img/36W-green.png"
+    },
+    {
+        src:"../img/36W-red.png"
+    },
+    {
+        src:"../img/46V-sand.png"
+    },
+    {
+        src:"../img/48V-grey-white.png"
+    },
+    {
+        src:"../img/36W-white.png"
+    },
+]
+
+
+// change-color
